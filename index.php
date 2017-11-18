decc83e994b08653e76a3928324776668e9f7f7f
Julian Kalitzke <julian.kalitzke@gmx.at>
2017-11-18 11:05:58 +0100

    Basic html setup

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 ?>
 
-<p>Index</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OpenProfile</title>
+        <meta charset="utf-8"/>
+        <link href="main.css" type="text/css">
+    </head>
+    <body>
+        <h1>OpenProfile</h1>
+        <div>
+
+        </div>
+    </body>
+</html>
