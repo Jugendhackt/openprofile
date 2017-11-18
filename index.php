7e0696b4f2402a21fd8c9098966845dcfc31e01f
Julian Kalitzke <julian.kalitzke@gmx.at>
2017-11-18 11:23:54 +0100

    index: removed header and div

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
-?>
 
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>OpenProfile</title>
         <meta charset="utf-8"/>
-        <link href="main.css" type="text/css">
+        <link href="main.css" rel="stylesheet"/>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+        <link
     </head>
     <body>
-        <h1>OpenProfile</h1>
-        <div>
 
-        </div>
     </body>
 </html>
