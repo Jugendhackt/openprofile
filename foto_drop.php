ecb6ea2f58136d849b63e0238e71b14107043f36
Xaver Kainz <xaver.kainz@aon.at>
2017-11-18 15:19:31 +0100

    Check file size an upload value

--- foto_drop.php
+++ foto_drop.php
@@ -1,7 +1,11 @@
 <?php
+ini_set("upload_max_filesize",2000000);
 $truePhoto = 1;
-if($_FILES["fileToUpload"]["error"] == 0){
+if($_FILES["fileToUpload"]['error'] == UPLOAD_ERR_OK){
     echo "file Uploaded";
+} elseif ($_FILES["fileToUpload"]['error'] == UPLOAD_ERR_INI_SIZE) {
+    echo "there was a Problem with the file size";
+    $truePhoto = 0;
 } else {
     echo "there was a Problem with uploading";
     $truePhoto = 0;
